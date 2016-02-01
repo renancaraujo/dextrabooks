88721e7801d144ab1839db1545d5961290eb3f66
Guilherme Gotardo <gui.gotardo@gmail.com>
2016-01-31 23:20:34 -0200

    dev

---
+++ books/src/test/java/br/com/dextra/utils/EndpointTestCase.java
@@ -0,0 +1,12 @@
+package br.com.dextra.utils;
+
+import io.yawp.testing.EndpointTestCaseBase;
+
+public class EndpointTestCase extends EndpointTestCaseBase {
+
+    @Override
+    protected String getAppPackage() {
+        return "br.com.dextra";
+    }
+
+}
