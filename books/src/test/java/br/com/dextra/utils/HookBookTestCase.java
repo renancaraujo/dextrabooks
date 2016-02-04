d60031eb8f98029726472ca8fb5ccdb94d97b72d
Julia Martins de Melo <julia.melo@dls98.lab.dextra.com.br>
2016-02-04 13:59:54 -0200

    testcase hook book

--- books/src/test/java/br/com/dextra/utils/HookBookTestCase.java
+++ books/src/test/java/br/com/dextra/utils/HookBookTestCase.java
@@ -1,14 +1,35 @@
 package br.com.dextra.utils;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.List;
 import org.junit.Test;
+import br.com.dextra.BaseTestCase;
+import br.com.dextra.endpoint.Book;
+import com.google.gson.JsonObject;
 
-public class HookBookTestCase {
+public class HookBookTestCase extends BaseTestCase {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddBook() {
+
+        JsonObject json = new JsonObject();
+        json.addProperty("name", "50 tons de cinza");
+        json.addProperty("author", "Stephen King");
+        json.addProperty("qtd", 5);
+
+        post("/book", json.toString());
+        
+        JsonObject json2 = new JsonObject();
+        json2.addProperty("name", "50 tons de cinza");
+        json2.addProperty("author", "Stephen King");
+        json2.addProperty("qtd", 12);
+
+        post("/book", json2.toString());
+        
+        List<Book> bookList = yawp(Book.class).where("name", "=", "50 tons de cinza").where("author", "=", "Stephen King").list();
+        
+        assertEquals(bookList.get(0).getQtd(), 17);
 	}
 
 }
