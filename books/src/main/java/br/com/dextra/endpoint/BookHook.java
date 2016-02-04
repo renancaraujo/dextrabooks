f45af338a14d26e2379a0d3832cc93818db8eb3a
Julia Martins de Melo <julia.melo@dls98.lab.dextra.com.br>
2016-02-04 10:35:19 -0200

    bookHook testado

--- books/src/main/java/br/com/dextra/endpoint/BookHook.java
+++ books/src/main/java/br/com/dextra/endpoint/BookHook.java
@@ -1,21 +1,22 @@
 package br.com.dextra.endpoint;
 
 import java.util.List;
-
-
 import io.yawp.repository.hooks.Hook;
 
 
 public class BookHook extends Hook<Book>{
 	@Override
 	public void beforeSave(Book book){
-		
 		List<Book> listbook = yawp(Book.class).where("name", "=", book.getName()).list();
-	
+		
 		if(!listbook.isEmpty()){
 			for (int i = 0; i < listbook.size(); i++) {
-				if (listbook.get(i).getAuthor() == book.getAuthor())
-					listbook.get(i).setQtd(listbook.get(i).getQtd()+1);
+				if (listbook.get(i).getAuthor().equals(book.getAuthor())) {
+					yawp.destroy(yawp(Book.class).fetch(listbook.get(i).getId()).getId());
+					int newQtd = listbook.get(i).getQtd()+book.getQtd();
+					book.setQtd(newQtd);
+				}
+
 			}
 		}
 	}
---
+++ books/src/test/java/br/com/dextra/utils/HookBookTestCase.java
@@ -0,0 +1,14 @@
+package br.com.dextra.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HookBookTestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
