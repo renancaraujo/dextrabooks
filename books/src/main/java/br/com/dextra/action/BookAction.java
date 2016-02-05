b62efb122a0e1ebebc310e726facc097e163ce26
Renan Matheus Correia Araujo <renan.araujo@dls79.lab.dextra.com.br>
2016-02-05 14:57:48 -0200

    listar emprestimos por livro

--- books/src/main/java/br/com/dextra/action/BookAction.java
+++ books/src/main/java/br/com/dextra/action/BookAction.java
@@ -1,23 +1,22 @@
 package br.com.dextra.action;
 
-import java.util.Map;
 
-import com.google.gson.Gson;
+import java.util.List;
 
+import io.yawp.commons.http.annotation.GET;
 import io.yawp.commons.http.annotation.POST;
 import io.yawp.repository.IdRef;
 import io.yawp.repository.actions.Action;
 import br.com.dextra.endpoint.Book;
 import br.com.dextra.endpoint.Loan;
 import br.com.dextra.endpoint.User;
-import br.com.dextra.exception.MyMessageException;
 import br.com.dextra.exception.NoBookAvaliableException;
 import br.com.dextra.exception.UserNotExistsException;
 
 public class BookAction extends Action<Book> {
 
 	@POST("borrow")
-	public boolean borrow(IdRef<Book> idLivro, User user) throws Exception {
+	public Loan borrow(IdRef<Book> idLivro, User user) throws Exception {
 
 		Book theBook = yawp(Book.class).fetch(idLivro);
 		try{
@@ -32,12 +31,17 @@ public class BookAction extends Action<Book> {
 
 			yawp.save(theBook);
 
-			return yawp.save(myLoan).isActive();
+			return yawp.save(myLoan);
 		}catch(RuntimeException e){
 			throw new UserNotExistsException("Usuario nao existe!");
 		}
 		
 
 		
+	}
+	
+	@GET("loans")
+	public List<Loan> LoanListUser(IdRef<Book>  book){
+		return yawp(Loan.class).where("idBook", "=", book).list();
 	}
 }
